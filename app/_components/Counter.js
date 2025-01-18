32473ee0690d88047a6c04c8b71038282e113d27
toyotadriver <polaemil@gmail.com>
2025-01-18 14:04:06 +0200

    first commit

---
+++ app/_components/Counter.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ data }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>There are {data.length} users</p>
+      <button onClick={() => setCount((count) => count + 1)}>+</button>
+      {count}
+    </div>
+  );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Cabins",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
+}
