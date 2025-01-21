38b9dc105ecf9acb6ab1f0a5c5da68eff7db6597
toyotadriver <polaemil@gmail.com>
2025-01-21 13:53:50 +0200

    "added interactions to SideNavigation"

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,6 +1,12 @@
-import Link from "next/link"
-import { CalendarDaysIcon, HomeIcon, UserIcon } from "@heroicons/react/24/solid"
-import SignOutButton from "./SignOutButton"
+"use client";
+import Link from "next/link";
+import {
+    CalendarDaysIcon,
+    HomeIcon,
+    UserIcon,
+} from "@heroicons/react/24/solid";
+import SignOutButton from "./SignOutButton";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
     {
@@ -18,16 +24,18 @@ const navLinks = [
         href: "/account/profile",
         icon: <UserIcon className="h-5 w-5 text-primary-600" />,
     },
-]
+];
 
 function SideNavigation() {
+    const pathname = usePathname();
+
     return (
         <nav className="border-r border-primary-900">
             <ul className="flex h-full flex-col gap-2 text-lg">
                 {navLinks.map((link) => (
                     <li key={link.name}>
                         <Link
-                            className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100`}
+                            className={`flex items-center gap-4 px-5 py-3 font-semibold text-primary-200 transition-colors hover:bg-primary-900 hover:text-primary-100 ${pathname === link.href ? "bg-primary-900" : ""}`}
                             href={link.href}
                         >
                             {link.icon}
@@ -41,7 +49,7 @@ function SideNavigation() {
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
 
-export default SideNavigation
+export default SideNavigation;
