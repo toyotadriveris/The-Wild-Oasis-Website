dae59e1c1f18cfbc21461b4f8e76bda4b41e7f3c
toyotadriver <polaemil@gmail.com>
2025-01-21 20:27:01 +0200

    "data fetching seems easy for some reason"

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,86 +1,42 @@
+import Cabin from "@/app/_components/Cabin";
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
 import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export async function generateMetadata({ params }) {
-    const { name } = (await getCabin(params.cabinId)) || "Loading...";
-    return { title: `Cabin ${name}` };
+  const { name } = (await getCabin(params.cabinId)) || "Loading...";
+  return { title: `Cabin ${name}` };
 }
 
 export async function generateStaticParams() {
-    const cabins = await getCabins();
+  const cabins = await getCabins();
 
-    const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
-    return ids;
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  return ids;
 }
 
 export default async function Page({ params }) {
-    const cabin = await getCabin(params.cabinId);
-    const {
-        id,
-        name,
-        maxCapacity,
-        regularPrice,
-        discount,
-        image,
-        description,
-    } = cabin;
+  const cabin = await getCabin(params.cabinId);
 
-    return (
-        <div className="mx-auto mt-8 max-w-6xl">
-            <div className="mb-24 grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 px-10 py-3">
-                <div className="relative -translate-x-3 scale-[1.15]">
-                    <Image
-                        fill
-                        className="object-cover"
-                        src={image}
-                        alt={`Cabin ${name}`}
-                    />
-                </div>
+  // const settings = await getSettings();
+  // const bookedDates = await getBookedDatesByCabinId(params.cabinId);
 
-                <div>
-                    <h3 className="mb-5 w-[150%] translate-x-[-254px] bg-primary-950 p-6 pb-1 text-7xl font-black text-accent-100">
-                        Cabin {name}
-                    </h3>
+  return (
+    <div className="mx-auto mt-8 max-w-6xl">
+      <Cabin cabin={cabin} />
+      <div>
+        <h2 className="mb-10 text-center text-5xl font-semibold text-accent-400">
+          Reserve {cabin.name} today. Pay on arrival.
+        </h2>
 
-                    <p className="mb-10 text-lg text-primary-300">
-                        <TextExpander>{description}</TextExpander>
-                    </p>
-
-                    <ul className="mb-7 flex flex-col gap-4">
-                        <li className="flex items-center gap-3">
-                            <UsersIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                                For up to{" "}
-                                <span className="font-bold">{maxCapacity}</span>{" "}
-                                guests
-                            </span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <MapPinIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                                Located in the heart of the{" "}
-                                <span className="font-bold">Dolomites</span>{" "}
-                                (Italy)
-                            </span>
-                        </li>
-                        <li className="flex items-center gap-3">
-                            <EyeSlashIcon className="h-5 w-5 text-primary-600" />
-                            <span className="text-lg">
-                                Privacy <span className="font-bold">100%</span>{" "}
-                                guaranteed
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div>
-                <h2 className="text-center text-5xl font-semibold">
-                    Reserve today. Pay on arrival.
-                </h2>
-            </div>
-        </div>
-    );
+        <Suspense fallback={<Spinner />}>
+          <Reservation cabin={cabin} />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
