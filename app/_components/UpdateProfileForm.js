de563998f1ff957eac3507474b61bbe85e12d6ae
toyotadriver <polaemil@gmail.com>
2025-01-23 15:03:08 +0200

    "this might be the simplest section in this next framework thing and its also amazing "

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,56 +1,74 @@
 "use client";
 
-import { useState } from "react";
+import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
-export default function UpdateProfileForm({ children }) {
-    const [count, setCount] = useState(0);
+export default function UpdateProfileForm({ guest, children }) {
+   const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
-    // CHANGE
-    const countryFlag = "pt.jpg";
-    const nationality = "portugal";
-    return (
-        <form className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg">
-            <div className="space-y-2">
-                <label>Full name</label>
-                <input
-                    disabled
-                    className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-                />
+   return (
+      <form
+         action={updateGuest}
+         className="flex flex-col gap-6 bg-primary-900 px-12 py-8 text-lg"
+      >
+         <div className="space-y-2">
+            <label>Full name</label>
+
+            <input
+               disabled
+               defaultValue={fullName}
+               name="fullName"
+               className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+            />
+         </div>
+
+         <div className="space-y-2">
+            <label>Email address</label>
+            <input
+               disabled
+               defaultValue={email}
+               name="email"
+               className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+            />
+         </div>
+
+         <div className="space-y-2">
+            <div className="flex items-center justify-between">
+               <label htmlFor="nationality">Where are you from?</label>
+               <img
+                  src={countryFlag}
+                  alt="Country flag"
+                  className="h-5 rounded-sm"
+               />
             </div>
+            {children}
+         </div>
 
-            <div className="space-y-2">
-                <label>Email address</label>
-                <input
-                    disabled
-                    className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
-                />
-            </div>
+         <div className="space-y-2">
+            <label htmlFor="nationalID">National ID number</label>
+            <input
+               defaultValue={nationalID}
+               name="nationalID"
+               className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
+            />
+         </div>
 
-            <div className="space-y-2">
-                <div className="flex items-center justify-between">
-                    <label htmlFor="nationality">Where are you from?</label>
-                    <img
-                        src={countryFlag}
-                        alt="Country flag"
-                        className="h-5 rounded-sm"
-                    />
-                </div>
-                {children}
-            </div>
+         <div className="flex items-center justify-end gap-6">
+            <Button />
+         </div>
+      </form>
+   );
+}
 
-            <div className="space-y-2">
-                <label htmlFor="nationalID">National ID number</label>
-                <input
-                    name="nationalID"
-                    className="w-full rounded-sm bg-primary-200 px-5 py-3 text-primary-800 shadow-sm"
-                />
-            </div>
+function Button() {
+   const { pending } = useFormStatus();
 
-            <div className="flex items-center justify-end gap-6">
-                <button className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
-            </div>
-        </form>
-    );
+   return (
+      <button
+         disabled={pending}
+         className="bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      >
+         {pending ? "UPDATING...." : "Update profile"}
+      </button>
+   );
 }
